d716b445d947f70a2df256dbf1c77aca69f90aa5
Jean Decian <jeandecian@hotmail.fr>
2024-06-11 09:22:19 -0400

    add new sizes to generator

--- random_files_generator_linux.sh
+++ random_files_generator_linux.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 head -c 1048576 /dev/urandom > files/1MB
+
+head -c 10485760 /dev/urandom > files/10MB
+head -c 26214400 /dev/urandom > files/25MB
+head -c 52428800 /dev/urandom > files/50MB
+head -c 78643200 /dev/urandom > files/75MB
+
 head -c 104857600 /dev/urandom > files/100MB
+head -c 262144000 /dev/urandom > files/250MB
+head -c 524288000 /dev/urandom > files/500MB
+head -c 786432000 /dev/urandom > files/750MB
+
 head -c 1073741824 /dev/urandom > files/1GB
+head -c 2147483648 /dev/urandom > files/2GB
+head -c 5368709120 /dev/urandom > files/5GB
